d5ddbe09adc3e7477beaf2c450b8f0b5c3dbd121
RealRajnish <rajnishf3@gmail.com>
2023-05-06 10:35:51 +0530

    static build added

--- server/src/app.js
+++ server/src/app.js
@@ -1,27 +1,32 @@
 const dotenv = require("dotenv");
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
 
 require("../db/conn");
 
 app.use(express.json());
 app.use(cookieParser());
 
-// we link the router files to make our route easy 
-app.use(require('../router/auth'));
+// we link the router files to make our route easy
+app.use(require("../router/auth"));
+
+app.use(express.static(path.join(__dirname, "../", "build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const PORT = process.env.PORT;
 
-
-// Middelware 
+// Middelware
 const middleware = (req, res, next) => {
-    console.log(`Hello my Middleware`);
-    next();
-}
-
+  console.log(`Hello my Middleware`);
+  next();
+};
 
 // app.get('/about', middleware, (req, res) => {
 //     console.log(`Hello my About`);
@@ -41,5 +46,5 @@ const middleware = (req, res, next) => {
 // });
 
 app.listen(PORT, () => {
-    console.log(`server is runnig at port no ${PORT}`);
-})
+  console.log(`server is runnig at port no ${PORT}`);
+});
